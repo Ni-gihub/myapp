396785611b08bdf27d73ceb1722607183df22056
NakaokuIkuya <kd1342595@st.kobedenshi.ac.jp>
2025-07-03 18:27:05 +0900

    レスポンシブ対応

--- app.js
+++ app.js
@@ -131,3 +131,12 @@ document.getElementById('search-btn').addEventListener('click', async () => {
     alert('ジオコーディングエラー: ' + error.message);
   }
 });
+
+
+
+const sidebar = document.getElementById('sidebar');
+const toggleBtn = document.getElementById('toggle-sidebar');
+
+toggleBtn.addEventListener('click', () => {
+  sidebar.classList.toggle('collapsed');
+});
